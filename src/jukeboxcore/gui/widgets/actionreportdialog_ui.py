f90dec705c1c529c2eb289fce1b7557bd83fa666
David Zuber <zuber.david@gmx.de>
2014-10-31 16:36:46 +0100

    cascading last section in actionreportdialog

--- src/jukeboxcore/gui/widgets/actionreportdialog_ui.py
+++ src/jukeboxcore/gui/widgets/actionreportdialog_ui.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'h:\projects\jukebox-core\src\jukeboxcore\gui\widgets\actionreportdialog.ui'
 #
-# Created: Fri Oct 31 15:08:39 2014
+# Created: Fri Oct 31 16:34:59 2014
 #      by: pyside-uic 0.2.15 running on PySide 1.2.2
 #
 # WARNING! All changes made in this file will be lost!
@@ -41,15 +41,15 @@ class Ui_ActionReportDialog(object):
         self.traceback_pte.setTextInteractionFlags(QtCore.Qt.TextSelectableByKeyboard|QtCore.Qt.TextSelectableByMouse)
         self.traceback_pte.setObjectName("traceback_pte")
         self.gridLayout.addWidget(self.traceback_pte, 3, 0, 1, 3)
-        self.checkBox = QtGui.QCheckBox(ActionReportDialog)
+        self.showtb_checkb = QtGui.QCheckBox(ActionReportDialog)
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Maximum, QtGui.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.checkBox.sizePolicy().hasHeightForWidth())
-        self.checkBox.setSizePolicy(sizePolicy)
-        self.checkBox.setChecked(False)
-        self.checkBox.setObjectName("checkBox")
-        self.gridLayout.addWidget(self.checkBox, 2, 2, 1, 1)
+        sizePolicy.setHeightForWidth(self.showtb_checkb.sizePolicy().hasHeightForWidth())
+        self.showtb_checkb.setSizePolicy(sizePolicy)
+        self.showtb_checkb.setChecked(False)
+        self.showtb_checkb.setObjectName("showtb_checkb")
+        self.gridLayout.addWidget(self.showtb_checkb, 2, 2, 1, 1)
         self.message_lb = QtGui.QLabel(ActionReportDialog)
         self.message_lb.setText("")
         self.message_lb.setWordWrap(True)
@@ -57,23 +57,28 @@ class Ui_ActionReportDialog(object):
         self.gridLayout.addWidget(self.message_lb, 1, 0, 1, 3)
         self.verticalLayout.addLayout(self.gridLayout)
         self.actions_tablev = QtGui.QTableView(ActionReportDialog)
+        self.actions_tablev.setSortingEnabled(True)
         self.actions_tablev.setObjectName("actions_tablev")
+        self.actions_tablev.horizontalHeader().setDefaultSectionSize(100)
+        self.actions_tablev.horizontalHeader().setStretchLastSection(True)
+        self.actions_tablev.verticalHeader().setStretchLastSection(False)
         self.verticalLayout.addWidget(self.actions_tablev)
         self.buttonBox = QtGui.QDialogButtonBox(ActionReportDialog)
         self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
         self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.Ok)
+        self.buttonBox.setCenterButtons(True)
         self.buttonBox.setObjectName("buttonBox")
         self.verticalLayout.addWidget(self.buttonBox)
 
         self.retranslateUi(ActionReportDialog)
         QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL("accepted()"), ActionReportDialog.accept)
         QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL("rejected()"), ActionReportDialog.reject)
-        QtCore.QObject.connect(self.checkBox, QtCore.SIGNAL("toggled(bool)"), self.traceback_pte.setVisible)
+        QtCore.QObject.connect(self.showtb_checkb, QtCore.SIGNAL("toggled(bool)"), self.traceback_pte.setVisible)
         QtCore.QMetaObject.connectSlotsByName(ActionReportDialog)
 
     def retranslateUi(self, ActionReportDialog):
         ActionReportDialog.setWindowTitle(QtGui.QApplication.translate("ActionReportDialog", "Report", None, QtGui.QApplication.UnicodeUTF8))
         self.status_title_lb.setText(QtGui.QApplication.translate("ActionReportDialog", "Status:", None, QtGui.QApplication.UnicodeUTF8))
         self.traceback_pte.setDocumentTitle(QtGui.QApplication.translate("ActionReportDialog", "Traceback", None, QtGui.QApplication.UnicodeUTF8))
-        self.checkBox.setText(QtGui.QApplication.translate("ActionReportDialog", "Show Traceback", None, QtGui.QApplication.UnicodeUTF8))
+        self.showtb_checkb.setText(QtGui.QApplication.translate("ActionReportDialog", "Show Traceback", None, QtGui.QApplication.UnicodeUTF8))
 
