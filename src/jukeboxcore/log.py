8904eb30f5c1deb1b60578d2170db62593a71b40
David Zuber <zuber.david@gmx.de>
2014-11-09 15:19:32 +0100

    setup root logger func

--- src/jukeboxcore/log.py
+++ src/jukeboxcore/log.py
@@ -1,10 +1,26 @@
 """ Here we provide standard loggers for our pipeline. It is advised to use them instead of the print function. """
-
 import logging
+import sys
 
 from jukeboxcore.constants import DEFAULT_LOGGING_LEVEL
 
-logging.basicConfig()
+
+def setup_root_logger():
+    """Setup the root logger with handlers
+
+    The logger default level is defined in the constants :data:`jukeboxcore.constants.DEFAULT_LOGGING_LEVEL` but can be overwritten by the environment variable \"JUKEBOX_LOG_LEVEL\"
+
+    :returns: None
+    :rtype: None
+    :raises: None
+    """
+    log = logging.getLogger()
+    handler = logging.StreamHandler(sys.stdout)
+    formatter = logging.Formatter()
+    handler.setFormatter(formatter)
+    log.addHandler(handler)
+    level = DEFAULT_LOGGING_LEVEL
+    log.setLevel(level)
 
 
 def get_logger(name, level=None):
@@ -21,8 +37,10 @@ def get_logger(name, level=None):
     The logger default level is defined in the constants :data:`jukeboxcore.constants.DEFAULT_LOGGING_LEVEL` but can be overwritten by the environment variable \"JUKEBOX_LOG_LEVEL\"
 
     """
-    if level is None:
-        level = DEFAULT_LOGGING_LEVEL
     log = logging.getLogger(name)
-    log.setLevel(level)
+    if level is not None:
+        log.setLevel(level)
     return log
+
+
+setup_root_logger()
