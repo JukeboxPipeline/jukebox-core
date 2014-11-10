79abd64a3da31ba800abe57bd4c40397913e8f98
David Zuber <zuber.david@gmx.de>
2014-11-10 11:08:36 +0100

    JP-81 #resolve logging now with jb top level logger

--- src/jukeboxcore/log.py
+++ src/jukeboxcore/log.py
@@ -5,8 +5,11 @@ import sys
 from jukeboxcore.constants import DEFAULT_LOGGING_LEVEL
 
 
-def setup_root_logger():
-    """Setup the root logger with handlers
+def setup_jukebox_logger():
+    """Setup the jukebox top-level logger with handlers
+
+    The logger has the name ``jukebox`` and is the top-level logger for all other loggers of jukebox.
+    It does not propagate to the root logger, because it also has a StreamHandler and that might cause double output.
 
     The logger default level is defined in the constants :data:`jukeboxcore.constants.DEFAULT_LOGGING_LEVEL` but can be overwritten by the environment variable \"JUKEBOX_LOG_LEVEL\"
 
@@ -14,9 +17,11 @@ def setup_root_logger():
     :rtype: None
     :raises: None
     """
-    log = logging.getLogger()
+    log = logging.getLogger("jb")
+    log.propagate = False
     handler = logging.StreamHandler(sys.stdout)
-    formatter = logging.Formatter()
+    fmt = "%(levelname)-8s:%(name)s:'%(message)s'"
+    formatter = logging.Formatter(fmt)
     handler.setFormatter(formatter)
     log.addHandler(handler)
     level = DEFAULT_LOGGING_LEVEL
@@ -26,7 +31,7 @@ def setup_root_logger():
 def get_logger(name, level=None):
     """ Return a setup logger for the given name
 
-    :param name: The name for the logger. It is advised to use __name__.
+    :param name: The name for the logger. It is advised to use __name__. The logger name will be prepended by \"jb.\".
     :type name: str
     :param level: the logging level, e.g. logging.DEBUG, logging.INFO etc
     :type level: int
@@ -37,10 +42,10 @@ def get_logger(name, level=None):
     The logger default level is defined in the constants :data:`jukeboxcore.constants.DEFAULT_LOGGING_LEVEL` but can be overwritten by the environment variable \"JUKEBOX_LOG_LEVEL\"
 
     """
-    log = logging.getLogger(name)
+    log = logging.getLogger("jb.%s" % name)
     if level is not None:
         log.setLevel(level)
     return log
 
 
-setup_root_logger()
+setup_jukebox_logger()
