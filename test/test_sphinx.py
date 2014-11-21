86eaa3d7a7202ca60682f40482e073411d3ec29d
mf114 <mf114@hdm-stuttgart.de>
2014-11-21 13:59:45 +0100

    added sphinx testwrapper

--- test/test_sphinx.py
+++ test/test_sphinx.py
@@ -1,16 +1,34 @@
 """ This file tests the sphinx-build """
 
 import py
+from os import path
 import subprocess
-def test_linkcheck(tmpdir):
-    doctrees = tmpdir.join("docs")
-    htmldir = tmpdir.join("dist/docs")
-    subprocess.check_call(
-        ["sphinx-build", "-b", "linkcheck",
-          "-d", str(doctrees), str(htmldir)])
-def test_build_docs(tmpdir):
-    doctrees = tmpdir.join("docs")
-    htmldir = tmpdir.join("dist/docs")
-    subprocess.check_call([
-        "sphinx-build", "-b", "html",
-          "-d", str(doctrees), str(htmldir)])
+
+
+class Test_SphinxBuild(object):
+    """Test sphinx build by wrapping sphinx build 
+       commands into a test class
+    """
+
+    def __init__(self, tmpdir):
+        """Setup environment variables
+
+        :param tmpdir: a pytest fixture pointing to a temp directory
+        """
+
+        self.doctreedir = path.join(__file__, "..", "docs") 
+        self.htmldir    = path.join(tmpdir, "dist", "docs") 
+
+    def test_linkcheck(self):
+        """performs a linkcheck on sphinx build """
+
+        subprocess.check_call(
+            ["sphinx-build", "-b", "linkcheck",
+              str(self.doctreedir), str(htmldir)])
+    
+    def test_build_docs(self):
+        """Performs a build check using sphinx build """
+
+        subprocess.check_call([
+            "sphinx-build", "-b", "html",
+              str(self.doctreedir), str(htmldir)])
