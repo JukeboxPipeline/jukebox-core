03806b5742a7f20d699cbc65de20a70a2fa066e1
David Zuber <zuber.david@gmx.de>
2014-11-28 17:25:05 +0100

    old fixtures added

--- test/conftest.py
+++ test/conftest.py
@@ -149,6 +149,139 @@ def djprj(setup_package, prjpath):
 
 
 @pytest.fixture(scope='session')
+def prj(setup_package, prjpath):
+    """Return project
+
+    name="Pixars Plants", short='plants', _path=prjpath, semester='SS14', scale="cm"
+    """
+    from jukeboxcore import djadapter as dj
+    return dj.projects.create(name="Pixars Plants", short='plants', _path=prjpath, semester='SS14', scale="cm")
+
+
+@pytest.fixture(scope='session')
+def seq(prj):
+    """Return sequence
+
+    name='Seq01', project=prj, description='plants everywhere'
+    """
+    from jukeboxcore import djadapter as dj
+    return dj.sequences.create(name='Seq01', project=prj, description='plants everywhere')
+
+
+@pytest.fixture(scope='session')
+def shot(prj, seq):
+    """Return shot
+
+    name='Shot01', project=prj, sequence=seq, description='closeup of plant'
+    """
+    from jukeboxcore import djadapter as dj
+    return dj.shots.create(name='Shot01', project=prj, sequence=seq, description='closeup of plant')
+
+
+@pytest.fixture(scope='session')
+def atype(prj):
+    """Return assettype
+
+    assettype is added to prj
+    name='matte', description='matte paintings'
+    """
+    from jukeboxcore import djadapter as dj
+    atype = dj.atypes.create(name='matte', description='matte paintings')
+    atype.projects.add(prj)
+    atype.save()
+    return atype
+
+
+@pytest.fixture(scope='session')
+def asset(prj, atype):
+    """Return asset
+
+    project=prj, atype=atype, name='piranha plant', description='eats mario'
+    """
+    from jukeboxcore import djadapter as dj
+    return dj.assets.create(project=prj, atype=atype, name='piranha plant', description='eats mario')
+
+
+@pytest.fixture(scope='session')
+def dep1(setup_package):
+    """Return a department
+
+    name="Design", short="des", assetflag=False
+    """
+    from jukeboxcore import djadapter as dj
+    return dj.departments.create(name="Design", short="des", assetflag=False)
+
+
+@pytest.fixture(scope='session')
+def dep2(setup_package):
+    """Return a department
+
+    name="Destruction", short="buum", assetflag=False
+    """
+    from jukeboxcore import djadapter as dj
+    return dj.departments.create(name="Destruction", short="buum", assetflag=False)
+
+
+@pytest.fixture(scope='session')
+def dep3(setup_package):
+    """Return a department
+
+    name="Matte", short="matte", assetflag=True
+    """
+    from jukeboxcore import djadapter as dj
+    return dj.departments.create(name="Matte", short="matte", assetflag=True)
+
+
+@pytest.fixture(scope='session')
+def task1(prj, shot, dep1):
+    """Return task
+
+    department=dep1, project=prj, status='New', element=shot
+    """
+    from jukeboxcore import djadapter as dj
+    return dj.tasks.create(department=dep1, project=prj, status='New', element=shot)
+
+
+@pytest.fixture(scope='session')
+def task2(prj, shot, dep2):
+    """Return task
+
+    department=dep2, project=prj, status='New', element=shot
+    """
+    from jukeboxcore import djadapter as dj
+    return dj.tasks.create(department=dep2, project=prj, status='New', element=shot)
+
+
+@pytest.fixture(scope='session')
+def task3(prj, asset, dep3):
+    from jukeboxcore import djadapter as dj
+    return dj.tasks.create(department=dep3, project=prj, status='New', element=asset)
+
+
+@pytest.fixture(scope='session')
+def tfile(task1, user):
+    """Return taskfile
+
+    task=task1, version=5, releasetype='handoff', path='anicepath', user=user, typ=dj.FILETYPES['mayamainscene']
+    """
+    from jukeboxcore import djadapter as dj
+    return dj.taskfiles.create(task=task1, version=5, releasetype='handoff', path='anicepath', user=user, typ=dj.FILETYPES['mayamainscene'])
+
+
+@pytest.fixture(scope='session')
+def taskfileinfo(tfile):
+    """Return a TaskFileInfo for the Taskfile of the taskfile fixture
+
+    task=task1, version=5, releasetype='handoff', user=user, typ=dj.FILETYPES['mayamainscene'], descriptor="desc1"
+    """
+    return TaskFileInfo(task=tfile.task,
+                        version=tfile.version,
+                        releasetype=tfile.releasetype,
+                        typ=tfile.typ,
+                        descriptor="desc1")
+
+
+@pytest.fixture(scope='session')
 def successf():
     """Return a function that will return a successful action status"""
     def func(f):
