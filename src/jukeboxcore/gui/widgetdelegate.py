49175429f6145431e03398966f39dda9fd5e83fe
David Zuber <zuber.david@gmx.de>
2015-01-04 20:57:38 +0100

    Delete pass statement

--- src/jukeboxcore/gui/widgetdelegate.py
+++ src/jukeboxcore/gui/widgetdelegate.py
@@ -501,7 +501,6 @@ class WD_TreeView(WidgetDelegateViewMixin, QtGui.QTreeView):
 
     By default the resize mode of the header will resize to contents.
     """
-    pass
 
     def __init__(self, *args, **kwargs):
         """Initialize a new treeview
